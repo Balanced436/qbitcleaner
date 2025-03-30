b3f31d0fbf6f786428d886b6d1667fed38721515
SAID <ismael.saidpro@protonmail.com>
2025-03-30 19:20:56 +0200

    [FEATURE] Update environment variables for qBittorrent integration and add validation checks

--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
-const QBIT_TORRENTS_INFOS_ENDPOINT = process.env.QBIT_TORRENTS_INFOS_ENDPOINT
-const QBIT_LOGIN_ENDPOINT = process.env.QBIT_LOGIN_ENDPOINT
-
+/**
+ * Read environment variables
+ */
+const QBIT_PORT = process.env.QBIT_PORT;
+const QBIT_IP = process.env.QBIT_IP;
+const QBIT_TRACKER_STATUS = process.env.QBIT_TRACKER_STATUS.toUpperCase();
+const QBIT_POLLING_INTERVAL = process.env.QBIT_POLLING_INTERVAL;
 const QBIT_USERNAME = process.env.QBIT_USERNAME
 const QBIT_PASSWORD = process.env.QBIT_PASSWORD
+
+
+/**
+ * Check that env variables are provided
+ */
+const requiredEnvVars = {
+    QBIT_IP: "the IP address of the qBittorrent instance",
+    QBIT_PORT: "the port of the qBittorrent instance",
+    QBIT_TRACKER_STATUS: "the tracker status to filter torrents",
+    QBIT_POLLING_INTERVAL: "the polling interval in minutes",
+    QBIT_USERNAME: "the username for qBittorrent authentication",
+    QBIT_PASSWORD: "the password for qBittorrent authentication"
+};
+
+/**
+ * Notify about missing environment variables
+ */
+for (const [key, description] of Object.entries(requiredEnvVars)) {
+    if (!process.env[key]) {
+        console.error(`Missing required environment variable: ${key}. Please set it to ${description}.`);
+        process.exit(1);
+    }
+}
+
+const QBIT_TORRENTS_INFOS_ENDPOINT = `http://${QBIT_IP}:${QBIT_PORT}/api/v2/torrents/info`
+const QBIT_LOGIN_ENDPOINT = `http://${QBIT_IP}:${QBIT_PORT}/api/v2/auth/login`
+
+console.info("QBIT_TORRENTS_INFOS_ENDPOINT", QBIT_TORRENTS_INFOS_ENDPOINT)
+console.info("QBIT_LOGIN_ENDPOINT", QBIT_LOGIN_ENDPOINT)
 
 /**
  * Logs in a user by sending their credentials to the specified api/v2/auth.
@@ -15,7 +48,6 @@ const login = async (username, password) => {
     const searchParams = new URLSearchParams()
     searchParams.append("username", username)
     searchParams.append("password", password)
-    console.info("searchParams", searchParams)
     const loginHeaders = new Headers()
     loginHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
@@ -44,8 +76,6 @@ const getTorrentInfos = async (cookies) => {
     console.info(`${new Date().toLocaleDateString('fr')}: getTorrentInfos`)
     const headers = new Headers
     headers.append("Cookie", cookies);
-    console.info("headers", headers)
-
     const options = {
         method: "GET",
         headers: headers,
@@ -60,6 +90,12 @@ const getTorrentInfos = async (cookies) => {
 
 setInterval(async () => {
     const { cookies } = await login(QBIT_USERNAME, QBIT_PASSWORD)
-    const torrentsInfos = await getTorrentInfos(cookies)
-    console.info(torrentsInfos)
-}, 5000)
+    const torrentsInfos = await getTorrentInfos(cookies);
+
+    if (!Array.isArray(torrentsInfos)) {
+        throw new Error("Expected torrentsInfos to be an array");
+    }
+
+    const filteredTorrents = torrentsInfos.filter((torrentInfo)=>torrentInfo.state.toUpperCase() === QBIT_TRACKER_STATUS)
+    console.info(filteredTorrents.map((torrent)=>torrent.name))
+}, QBIT_POLLING_INTERVAL * 1000)
