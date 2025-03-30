f03d211d19c47340bcff19ef7484e1fac85edf28
SAID <ismael.saidpro@protonmail.com>
2025-03-30 11:08:29 +0200

    [FEATURE] Add getTorrentInfos function

--- src/index.js
+++ src/index.js
@@ -4,23 +4,29 @@ const QBIT_LOGIN_ENDPOINT = process.env.QBIT_LOGIN_ENDPOINT
 const QBIT_USERNAME = process.env.QBIT_USERNAME
 const QBIT_PASSWORD = process.env.QBIT_PASSWORD
 
-const searchParams = new URLSearchParams()
-searchParams.append("username",QBIT_USERNAME)
-searchParams.append("password",QBIT_PASSWORD)
-console.info("searchParams",searchParams)
-const loginHeaders = new Headers()
-loginHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+/**
+ * Logs in a user by sending their credentials to the specified api/v2/auth.
+ *
+ * @param {string} username - The username of the user.
+ * @param {string} password - The password of the user.
+ */
+const login = async (username, password) => {
+    console.info(`${new Date().toLocaleDateString('fr')}: login`)
+    const searchParams = new URLSearchParams()
+    searchParams.append("username", username)
+    searchParams.append("password", password)
+    console.info("searchParams", searchParams)
+    const loginHeaders = new Headers()
+    loginHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
-const logionOptions = {
-    method: "POST",
-    headers: loginHeaders,
-    body: searchParams,
-    redirect: "follow"
+    const logionOptions = {
+        method: "POST",
+        headers: loginHeaders,
+        body: searchParams,
+        redirect: "follow"
 
-}
-
-const login = async (options) => {
-    const response = await fetch(`${QBIT_LOGIN_ENDPOINT}`, options);
+    }
+    const response = await fetch(`${QBIT_LOGIN_ENDPOINT}`, logionOptions);
     if (response.ok) {
         const cookies = response.headers.get('set-cookie');
         return { response, cookies };
@@ -29,6 +35,31 @@ const login = async (options) => {
 };
 
 
-setInterval(async ()=>{
-    await login(logionOptions)
-},5000)
+/**
+ * Get torrentInfos
+ *
+ * @param {string} cookies
+ */
+const getTorrentInfos = async (cookies) => {
+    console.info(`${new Date().toLocaleDateString('fr')}: getTorrentInfos`)
+    const headers = new Headers
+    headers.append("Cookie", cookies);
+    console.info("headers", headers)
+
+    const options = {
+        method: "GET",
+        headers: headers,
+    }
+    const response = await fetch(`${QBIT_TORRENTS_INFOS_ENDPOINT}`, options)
+    if (response.ok) {
+        return await response.json()
+    } else {
+        throw Error("Get torrents infos failed")
+    }
+}
+
+setInterval(async () => {
+    const { cookies } = await login(QBIT_USERNAME, QBIT_PASSWORD)
+    const torrentsInfos = await getTorrentInfos(cookies)
+    console.info(torrentsInfos)
+}, 5000)
